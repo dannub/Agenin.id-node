ac86a9b5f4bbe271fdb273ffb9b9b7f0c9de9d67
dannub <fardannubimantara@gmail.com>
2020-05-31 10:16:46 +0700

    update all (18)

--- index.js
+++ index.js
@@ -24,12 +24,12 @@ app.use(express.json());
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested,Content-Type,Accept,Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Expose-Headers', 'Content-Length');
     if(req.method ==='OPTIONS'){
       res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-      return res.status(200).json{};
+      return res.status(200).json({});
     }
     next();
   });
