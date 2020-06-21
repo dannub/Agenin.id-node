8559daed16347709f1aa2b174aa3a03edcb50387
dannub <fardannubimantara@gmail.com>
2020-06-22 00:35:33 +0700

    update for all

--- routes/User/item/Cart.js
+++ routes/User/item/Cart.js
@@ -227,8 +227,56 @@ router.post('/create/:productId',verify,async(req,res)=>{
                                 { $push: { my_carts: cart } }
                                 ,
                                 { upsert: true, new: true }
-                            );
-                            res.status(200).json(addCart)
+                            ) .exec(async(err, result) => {
+                                if (err) throw res.status(400).json({message: err});
+                                try {
+                                    //All Cart
+                            
+                                    await User.aggregate([
+                                        {"$unwind":"$my_carts"}, 
+                                        {
+                                            $match: { _id : mongoose.Types.ObjectId(req.params.userId) }
+                                          },
+                                        { $replaceRoot: { newRoot:"$my_carts"}},
+                                        { $project: {   date:0} },
+                                        {
+                                            $lookup:
+                                                {
+                                                    from: "products",
+                                                    let: { product_ID: "$product_ID"},
+                                                    pipeline: [
+                                                        { $match:
+                                                           { 
+                                                                $expr:
+                                                                   { $eq: [ "$incharge",  "$$product_ID" ] }
+                                                            
+                                                           }
+                                                        },
+                                                        { $project: {  title_product: 1,image : {'$arrayElemAt': ['$image', 0] },price:1,cutted_price:1,satuan:1 } }
+                                                    ],
+                                                    as: "product"
+                                                }
+                                        },
+                                        { $project: {  product_ID: 0} },
+                                        {
+                                            $group: {
+                                               _id: null,
+                                               count: { $sum: 1 }
+                                            }
+                                          }
+                                        
+                                    ])
+                                    .exec((err, result) => {
+                                        if (err) throw res.status(400).json({message: err});
+                                        res.status(200).json(result[0].count)
+                                    });
+                                    //res.status(200).json(topdeals[0].top_deals)
+                               
+                                } catch (error) {
+                                    res.status(400).json({message: error})
+                                }
+                            });
+                          
                         } catch (error) {
                             res.status(400).json({message: error})
                         }
@@ -346,9 +394,80 @@ router.delete('/delete/:cartId',verify,async(req,res,next)=>{
             _id : req.params.userId
         },
         { $pull: { my_carts: {  _id: mongoose.Types.ObjectId(req.params.cartId)}  } }
-        ,{ multi: true })
-
-        res.status(200).json(deleteCart)
+        ,{ multi: true }) .exec(async(err, result) => {
+            if (err) throw res.status(400).json({message: err});
+            
+                await User.aggregate([
+                  {"$unwind":"$my_carts"}, 
+                  {
+                      $match: { _id : mongoose.Types.ObjectId(req.params.userId) }
+                    },
+                   
+                  { $replaceRoot: { newRoot: { $mergeObjects: [ { user_id: "$_id"},"$my_carts" ] }}},
+                  { $project: {   date:0} },
+                  {
+                      $lookup:
+                          {
+                              from: "products",
+                              let: { product_ID: "$product_ID"},
+                              pipeline: [
+                                  { $match:
+                                     { 
+                                          $expr:
+                                             { $eq: [ "$incharge",  "$$product_ID" ] }
+                                      
+                                     }
+                                  },
+                                  { $project: {  title_product: 1,image : {'$arrayElemAt': ['$image', 0] },price:1,in_stock:1,cutted_price:1,satuan:1,min_order:1,average_rating:1,berat:1  } }
+                              ],
+                              as: "product"
+                          }
+                  },
+                  { $project: {  product_ID: 0} },
+                      { 
+                          "$group": {
+                              _id : "$user_id",
+                              user_id: { "$first": "$user_id" },
+                              carts: { $push: "$$ROOT"},
+                          }
+                      },
+                      {
+                          "$addFields": {
+                              "carts":   { $cond : [ { $eq : [ "$carts", [] ] },"$$REMOVE", 
+                               {
+                                "$map": {
+                                  "input": "$carts",
+                                  "as": "c",
+                                  "in": {
+                                      "_id": '$$c._id',
+                                      "product_ID": {'$arrayElemAt': ['$$c.product._id', 0] } ,
+                                      "in_stock":{'$arrayElemAt': ['$$c.product.in_stock', 0] },
+                                      "jumlah": '$$c.jumlah',
+                                      "title_product": {'$arrayElemAt': ['$$c.product.title_product', 0] },
+                                      "price":  {'$arrayElemAt': ["$$c.product.price", 0] } ,
+                                      "cutted_price":  {'$arrayElemAt': ["$$c.product.cutted_price", 0] }  ,
+                                      "average_rating":  {'$arrayElemAt': ["$$c.product.average_rating", 0] }  ,
+                                      "satuan":  {'$arrayElemAt': ["$$c.product.satuan", 0] },
+                                      "min_order":  {'$arrayElemAt': ["$$c.product.min_order", 0] },
+                                      "berat":  {'$arrayElemAt': ["$$c.product.berat", 0] },
+                                      "image":  {'$arrayElemAt': ["$$c.product.image", 0] } ,
+                                  
+                                  }
+                                }
+                              } ]}
+                          }
+                      },
+                      { $project: {  _id: 0} }
+      
+      
+              ])
+              .exec((err, result) => {
+                  if (err) throw res.status(400).json({message: err});
+                  res.status(200).json(result[0])
+              });
+              //res.status(200).json(topdeals[0].top_deals)
+         
+        });
     
    
     } catch (error) {
